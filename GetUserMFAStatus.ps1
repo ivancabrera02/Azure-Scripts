74a287a5973cbe6a09bebaac86dcab36464c2596
Iván Cabrera <103500562+ivancabrera02@users.noreply.github.com>
2024-08-19 16:12:14 +0200

    Add files via upload

---
+++ GetUserMFAStatus.ps1
@@ -0,0 +1,12 @@
+﻿# Obtener todos los usuarios de Azure AD
+$users = az ad user list --query "[].{UserPrincipalName:userPrincipalName, MFAEnabled:strongAuthenticationMethods}" --output json | ConvertFrom-Json
+
+# Verificar MFA para cada usuario
+foreach ($user in $users) {
+    $mfaEnabled = $user.MFAEnabled.Count -gt 0
+    if ($mfaEnabled) {
+        Write-Host "El usuario $($user.UserPrincipalName) tiene MFA habilitado."
+    } else {
+        Write-Host "El usuario $($user.UserPrincipalName) NO tiene MFA habilitado."
+    }
+}
